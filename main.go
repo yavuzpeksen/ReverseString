d9d2cfab72060eeab100c05a8d9decaa5ab7b9c7
Yavuz <yavuzpeksen1986@hotmail.com>
2018-06-19 15:55:17 +0300

    randgenerators package is added

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/yavuzpeksen/stringutil"
+	"github.com/muratsplat/randgenerators"
 )
 
 func main() {
-	fmt.Printf(stringutil.Reverse("!gnaL oG ,olleH"))
+	
+	fmt.Println(stringutil.Reverse("!gnaL oG ,olleH"))
+	
+	var size uint = 5
+	random, err := randgenerators.GenereteRandString(size, randgenerators.UPPER_CASE_ALPHA, randgenerators.LOWER_CASE_ALPHA, randgenerators.NUMBER)
+	if err == nil{
+		fmt.Println("Generated string: ", random) 
+	}
+	
 }
 
